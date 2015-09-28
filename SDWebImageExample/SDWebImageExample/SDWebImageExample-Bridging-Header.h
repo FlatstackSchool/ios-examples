30b3f46e784b3e36419200460c93b2e6291b4b8f
VladimirGoncharov <vladimir1631@yandex.ru>
2015-09-28 18:54:43 +0400

    Add SDWebImage Example

---
+++ SDWebImageExample/SDWebImageExample/SDWebImageExample-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SDWebImageExample-Bridging-Header.h
+//  SDWebImageExample
+//
+//  Created by Vladimir Goncharov on 16.09.15.
+//  Copyright © 2015 FlatStack. All rights reserved.
+//
+
+#import <SDWebImage/SDWebImageManager.h>
+#import <SDWebImage/UIImageView+WebCache.h>
+#import <SDWebImage/SDWebImagePrefetcher.h>
+#import <SDWebImage/SDWebImageDownloaderOperation.h>
+#import <SDWebImage/UIImage+GIF.h>
---
+++ SDWebImageExample/SDWebImageExample/SDWebImageExample/SDWebImageExample/SDWebImageExample/SDWebImageExample/SDWebImageExample/SDWebImageExample/MasterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.swift
+//  SDWebImageExample
+//
+//  Created by Vladimir Goncharov on 16.09.15.
+//  Copyright © 2015 FlatStack. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+}
